bec963f28d79863078bfc61c7b228bed004b4047
Carlos Santana <csantana@us.ibm.com>
2017-10-06 21:30:50 -0400

    update demo5

--- demo5/run.sh
+++ demo5/run.sh
@@ -6,21 +6,32 @@ echo "Using CLI ${WSKCLI}"
 WSKURL=`${WSKCLI} action get demo5/dbInsert --url | tail -n 1`
 echo ${WSKURL}
 #curl ${WSKURL}
-WSKAPIGWURL=`bx wsk api list /db /friends post -f | grep URL | awk -FURL: '{print $2}'`
-curl -H "Content-Type: application/json" -X POST -d '{"handle":"raymondcamden","name":"Raymond","project":"Cats"}' $WSKAPIGWURL
+WSKAPIGWURL=`${WSKCLI} api list /db /friends post -f | grep URL | awk -FURL: '{print $2}'`
+curl -H "Content-Type: application/json" \
+-X POST -d '{"handle":"csantanapr","name":"Carlos","project":"OpenWhisk"}' \
+$WSKAPIGWURL
+curl -H "Content-Type: application/json" \
+-X POST -d '{"handle":"raymondcamden","name":"Raymond","project":"Cat Pics"}' \
+$WSKAPIGWURL
+curl -H "Content-Type: application/json" \
+-X POST -d '{"handle":"macdonst","name":"Simon","project":"PhoneGap"}' \
+$WSKAPIGWURL
+curl -H "Content-Type: application/json" \
+-X POST -d '{"handle":"johnwargo","name":"John","project":"VSCode"}' \
+$WSKAPIGWURL
 
 WSKURL=`${WSKCLI} action get demo5/dbGet --url | tail -n 1`
 echo ${WSKURL}
 #curl ${WSKURL}
 #curl ${WSKURL}?handle=csantanapr
-WSKAPIGWURL=`bx wsk api list /db /friends get -f | grep URL | awk -FURL: '{print $2}'`
-curl $WSKAPIGWURL
+WSKAPIGWURL=`${WSKCLI} api list /db /friends get -f | grep URL | awk -FURL: '{print $2}'`
+curl -s $WSKAPIGWURL | jq .
 
 WSKURL=`${WSKCLI} action get demo5/dbDelete --url | tail -n 1`
 echo ${WSKURL}
 #curl ${WSKURL}
 #curl ${WSKURL}?handle=csantanapr
-WSKAPIGWURL=`bx wsk api list /db /friends delete -f | grep URL | awk -FURL: '{print $2}'`
-curl -X DELETE $WSKAPIGWURL?handle=raymondcamden
+WSKAPIGWURL=`${WSKCLI} api list /db /friends delete -f | grep URL | awk -FURL: '{print $2}'`
+curl -s -X DELETE $WSKAPIGWURL?handle=csantanapr | jq .
 
 
